13f2a14c4aecc01f50e47b9de5fcf602b019fb7a
arteficent <sourabh9312324@gmail.com>
2021-12-21 18:26:11 +0530

    final

--- firebase.js
+++ firebase.js
@@ -2,15 +2,16 @@ import firebase from "firebase";
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-  };
+  apiKey: "AIzaSyCvB5znLOuO30HR8URt4KQH8UXhyBRONtQ",
+  authDomain: "diveassignment-bc30d.firebaseapp.com",
+  projectId: "diveassignment-bc30d",
+  storageBucket: "diveassignment-bc30d.appspot.com",
+  messagingSenderId: "28944741380",
+  appId: "1:28944741380:web:0f18ae1b58a66e5f03f151",
+  measurementId: "G-D6LK23S4NP"
+};
   
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
